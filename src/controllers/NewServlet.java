116b22a37b0b0832a46f22bf4361cf604271fca6
takashiono2 <takashiono710@gmail.com>
2022-10-31 00:31:40 +0900

    Index/NewServletの修正

---
+++ src/controllers/NewServlet.java
@@ -0,0 +1,35 @@
+package controllers;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Message;
+
+
+@WebServlet("/new")
+public class NewServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    
+    public NewServlet() {
+        super();
+    }
+
+    
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+        request.setAttribute("_token", request.getSession().getId());
+
+        
+        request.setAttribute("message", new Message());
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/new.jsp");
+        rd.forward(request, response);
+     }
+}
